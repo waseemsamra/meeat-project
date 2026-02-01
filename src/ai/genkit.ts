7fdb182b7d182e853e0b1dad3e7208d87be50899
Waseem Samra <waseemsamra@gmail.com>
2026-02-01 20:56:52 +0000

    error when curat for me in box making

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,7 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import { config } from 'dotenv';
+config();
 
 export const ai = genkit({
   plugins: [googleAI()],
